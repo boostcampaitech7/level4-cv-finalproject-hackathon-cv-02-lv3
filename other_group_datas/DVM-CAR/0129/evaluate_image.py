90bf01f5a58184e548131d3f348270d7ea070357
Dongjin <jinlee.april@gmail.com>
2025-01-29 03:23:29 +0000

    Chore : backup

--- other_group_datas/DVM-CAR/0129/evaluate_image.py
+++ other_group_datas/DVM-CAR/0129/evaluate_image.py
@@ -4,6 +4,11 @@ import os
 import re
 import pandas as pd
 from tqdm import tqdm
+import signal, time
+
+def timeout_handler():
+    print("timeout")
+
 
 def validate_path(path):
     validate_path = ""
@@ -25,30 +30,48 @@ def chat(model, instruction, image_path=None):
     content = response['message']['content']
     return content
 
-def get_score_from_chat(model, instruction, image_path=None, pattern = r"\d\.\d+"):
-    n_max_trial = 10 # score을 찾지 못했을 때 최대 시도 횟수
+def find_score_from_response(pattern, response):
+    matches = re.findall(pattern, response)
+    score = -1
+
+    if matches: # 일치하는 패턴 찾음
+        try:
+            score = float(matches[-1]) # 마지막 매칭 결과를 score로 반영
+        except:
+            pass
+
+    return score
+
+def get_score_from_chat(model, instruction, image_path=None, pattern = r"\d\.\d{4}", timeout=1):
+    n_max_trial = 20 # score을 찾지 못했을 때 최대 시도 횟수
     score = -1
 
     for _ in range(n_max_trial):
-        response = chat(model, instruction, image_path)
-        search = re.search(pattern, response)
-
-        if search: # 일치하는 패턴 찾음
-            try:
-                score = float(search.group())
-            except:
-                pass
+        signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(timeout)
+        
+        try: 
+            response = chat(model, instruction, image_path)
+            score = find_score_from_response(pattern, response)
+        finally:
+            signal.alarm(0)
             
-            if (0 <= score and score <= 1): # 정상적인 score 값이면 종료
-                break
+        if (0 <= score and score <= 1): # 정상적인 score 값이면 종료
+            break
     
     response = response.replace("\n", "") # 엔터 제거
     response = response.replace("\r", "") # 엔터 제거
     return score, response
 
 
+def write_file(path, content):
+    with open(path, 'a') as f:
+        f.write(content)
+        f.flush()
+
+
 if __name__ == '__main__':
-    model = 'llava:34b'
+    model = 'minicpm-v'
     image_dir_path = '../data/confirmed_fronts'
     save_dir_path = f'result/{model}/stylishness'
     years = list(range(2018, 1990, -1))
@@ -63,7 +86,7 @@ if __name__ == '__main__':
         )
 
     save_dir_path = validate_path(save_dir_path)
-    os.makedirs(save_dir_path, exist_ok=True)
+    os.makedirs(save_dir_path, exist_ok=True) # 폴더 생성
 
     for year in years:
         print(f"***** {year}년 확인 중 *****")
@@ -75,16 +98,16 @@ if __name__ == '__main__':
 
         if os.path.exists(save_path): # 이미 존재하는 경로이면 무시
             continue
+        write_file(save_path, "")
 
-        for image_path in tqdm(image_paths):
+        for image_path in tqdm(image_paths, dynamic_ncols=False):
             rel_image_path = image_path.replace(image_dir_path + '/', "")
             score, response = get_score_from_chat(model=model, instruction=instruction, image_path=image_path)
             df.loc[len(df)] = [rel_image_path, score, response]
-            tqdm.write(f"{year} - {rel_image_path} - score: {score:.4f}")
-            
-            
+            tqdm.write(f"{rel_image_path} - score: {score:.4f}")
+
+        # 파일 저장
         df.to_csv(save_path, index=False)
-        with open(save_instruction_path, 'w') as f:
-            f.write(instruction)
-            f.flush()
+        write_file(save_instruction_path, instruction)
+        
         
