af05d178c4bf09162ff7bff8914ba7106c5f3001
stop0729 <ksw5464980@naver.com>
2025-02-10 12:45:00 +0000

    Revise

--- search_final.py
+++ search_final.py
@@ -1,9 +1,13 @@
 from bayes_opt import BayesianOptimization
 from bayes_opt.util import UtilityFunction
 import numpy as np
+import joblib
 import pandas as pd
+import argparse  # 실행 모드 선택을 위해 추가
 import time  # 실행 시간 측정을 위한 모듈
 from joblib import Parallel, delayed
+from shapely.geometry import Point, Polygon
+from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -50,7 +54,9 @@ def search(X_train, y_train, model, search_x, search_y):
             priority_list[i]=[search_x[i]["순위"], search_x[i]['목표']]
         
         range_dict[i]=search_x[i]["범위 설정"]
-    
+
+
+
     # single + multi 합치기 목표
     # multi의 경우에 search_x에서 다 최적화 않기만 있을때는 y만 target -> single
     # 이중반복문 search_x를 최적화 하지 않을때, search_y의 목표가 범위에 맞추기인지, 최소환지 최대환지, 목표값 만주긴지
@@ -85,8 +91,32 @@ def search(X_train, y_train, model, search_x, search_y):
                 else:
                     return - abs(search_y[y]['범위 설정'].sum()-2* prediction)
                 
-
         
+        # #정규화 없는 버전
+
+        # else:
+            
+        #     max_num = max(map(lambda x: x[0], priority_list.values()))
+
+        #     target = calculate(row, priority_list, max_num, initial_y, search_y, y)
+            
+        #     def objective_function(**kwargs):
+
+        #         X_simulation = row.copy()  # 현재 행을 복사하여 사용
+
+        #         for key, value in kwargs.items():
+        #             X_simulation[key] = value  # ✅ 각 Feature의 값을 업데이트
+                
+        #         # 데이터를 모델에 맞는 포맷으로 변환
+        #         input_df = pd.DataFrame([X_simulation])
+        #         prediction = model.predict(input_df)[0]  # 1개의 값 예측
+
+        #         target = calculate(X_simulation, priority_list, max_num, prediction, search_y, y)
+                
+        #         return target
+            
+
+
         # 0~1 정규화 아무런 변화가 없음
         else:
 
@@ -126,9 +156,48 @@ def search(X_train, y_train, model, search_x, search_y):
                 target = calculate(X_simulation_scaled, priority_list, max_num, prediction, search_y, y)
                 
                 return target
+
+
+
+        # # y기준으로 정규화
+        # else:
+
+
+        #     y_min, y_max = y_train.min(), y_train.max()
+
+        #     # 각 feature의 최소, 최대 값 저장
+        #     feature_ranges = {
+        #         col: (X_train[col].min(), X_train[col].max()) for col in X_train.columns
+        #     }
+
             
+        #     max_num = max(map(lambda x: x[0], priority_list.values()))
+        #     target = calculate(row, priority_list, max_num, initial_y, search_y, y)
+            
+        #     def objective_function(**kwargs):
 
+        #         X_simulation = row.copy()  # 현재 행을 복사하여 사용
+        #         X_simulation_scaled = X_simulation.copy()  # 마찬가지로 복사하는데 정규화된 값 저장용
 
+        #         for key, value in kwargs.items(): 
+        #             if key in feature_ranges:
+        #                 X_min, X_max = feature_ranges[key]
+        #                 if (X_max - X_min) > 0:  # 분모가 0이 되는 경우 방지
+        #                     X_scaled = (value - X_min) / (X_max - X_min) # X를 0~1로 정규화
+        #                     X_simulation_scaled[key] = X_scaled * (y_max - y_min) + y_min # y 범위로 변환
+        #                 else:
+        #                     X_simulation_scaled[key] = value  # 변화가 없는 경우 그대로 사용
+        #                 X_simulation[key] = value
+                
+        #         # 데이터를 모델에 맞는 포맷으로 변환
+        #         input_df = pd.DataFrame([X_simulation])
+        #         prediction = model.predict(input_df)[0]  # 1개의 값 예측
+
+        #         # prediction = (prediction - y_min) / (y_max - y_min) y를 0~로 정규화        
+
+        #         target = prediction
+                
+        #         return target
             
 
         # Bayesian Optimization 실행
@@ -142,9 +211,14 @@ def search(X_train, y_train, model, search_x, search_y):
         )
 
 
+        # # 기존 X_simulation의 값을 초기 값으로 설정
+        # optimizer.register(
+        #     params={i : row[i] for i in search_x.keys()}, 
+        #     target=target  # 초기 가격 값을 Bayesian Optimization에 등록  ## 이렇게 해도 되나?
+        # )
+
         utility = UtilityFunction(kind="ei", xi=0.1)
         optimizer.maximize(init_points=10, n_iter=50, acquisition_function=utility) #acquisition_function=utility
-        
         # 최적의 결과 저장
         best_solution = optimizer.max['params']
         # 원본 데이터(row) 기반으로 새로운 DataFrame 생성 (최적화하지 않은 변수 포함)
@@ -163,6 +237,8 @@ def search(X_train, y_train, model, search_x, search_y):
             'y': y_pred  # 모델이 최적 x 값에서 예측한 y 값 추가
         }
         return {**dict1,**dict2}
+    
+    
     # 병렬 처리 실행 (n_jobs=-1: 모든 CPU 코어 사용)
     optimal_solutions = Parallel(n_jobs=-1, backend="loky")(
         delayed(optimize_row)(index, row) for index, row in X_train.iterrows()
